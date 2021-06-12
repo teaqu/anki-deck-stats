4d49e5782c146e202eec6c386cdfb4e041ab9d42
teaqu <583418+teaqu@users.noreply.github.com>
2021-06-12 14:17:37 +0100

    Fix #8

--- deck_stats/__init__.py
+++ deck_stats/__init__.py
@@ -5,11 +5,10 @@ from aqt import mw
 
 # Replace _renderStats method
 def renderStats(self, _old):
-    # Count number of cards that match each search term
-    query = mw.col.build_search_string("is:due -is:suspended -is:buried")
-    due = len(mw.col.find_cards(query))
-    query = mw.col.build_search_string("is:new -is:suspended -is:buried")
-    new = len(mw.col.find_cards(query))
+    due = new = 0
+    for tree in self.mw.col.sched.deckDueTree():
+        due += tree[2] + tree[3]
+        new += tree[4]
 
     # Setup data to print
     return _("Due") + ": <font color=#0a0> %(c)s </font>" % dict(c=due) \
